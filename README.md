e2f6399d3f9fbb2eca4f6d2a77a33638e214200f
chengfu <chengfu.wcy@antfin.com>
2017-11-26 10:06:12 +0800

    ADD: pptbox related files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # git-tutorial
+
 A basic tutorial for git
+
+Thanks `PPTBox`
---
+++ pptbox/pptbox.js
@@ -0,0 +1,168 @@
+(function() {
+    function PPTBox(options, container) {
+        this.options = {
+            animate: 'slide',
+            speed: 'normal'
+        }
+        this.options.animate = options.animate || this.options.animate;
+        this.options.speed = options.speed || this.options.speed;
+        this.getSpeed(this.options.speed);
+        this.pageStyle = container.querySelector('section').className + ' ';
+        var preButton = document.createElement('div');
+        var nextButton = document.createElement('div');
+        preButton.innerHTML = '&lt;';
+        nextButton.innerHTML = '&gt;';
+        preButton.className = 'pre-button';
+        nextButton.className = 'next-button';
+        container.appendChild(preButton);
+        container.appendChild(nextButton);
+        this.el = {
+            container: container,
+            boxList: container.querySelectorAll('section'),
+            preButton: preButton,
+            nextButton: nextButton
+        };
+        this.attr = {
+            _length: this.el.boxList.length, // 总页数
+            current: 0, // 展示页索引
+            next: 1, // 下一页索引
+            pre: -1 // 上一页索引
+        };
+        this.init();
+    }
+    PPTBox.prototype = {
+        init: function() { //
+            var boxList = this.el.boxList;
+            boxList[this.attr.current].className += ' show ';
+            var maxHeight = boxList[0].clientHeight;
+            for (var i = 0; i < boxList.length; i++) {
+                boxList[i].style.animationDuration = this.options.speed + 'ms';
+                if(boxList[i].clientHeight>maxHeight) maxHeight = boxList[i].clientHeight;
+            }
+            this.el.container.style.height = maxHeight+'px';
+            this.setArrow();
+            this.bindEvent();
+        },
+        bindEvent: function() {
+            var self = this;
+            this.el.container.querySelector('.pre-button').addEventListener('click', function(e) {
+                self.animate(0, self.options.animate);
+            });
+            this.el.container.querySelector('.next-button').addEventListener('click', function(e) {
+                self.animate(1, self.options.animate);
+            });
+
+            var boxList = this.el.boxList;
+            var startX, distanceX, isDown = false,
+                isSwipe = false;
+            for (var i = 0; i < boxList.length; i++) {
+                var swipeWidth = boxList[i].clientWidth / 15;
+                boxList[i].addEventListener('touchstart', function(e) {
+                    isDown = true;
+                    startX = e.touches[0].pageX;
+                });
+                boxList[i].addEventListener('touchmove', function(e) {
+                    if (isDown) {
+                        distanceX = e.touches[0].pageX - startX;
+                        Math.abs(distanceX) > swipeWidth ? isSwipe = true : isSwipe = false;
+                    }
+                });
+                boxList[i].addEventListener('touchend', function(e) {
+                    if (isSwipe) {
+                        distanceX > 0 ? self.animate(0, self.options.animate) : self.animate(1, self.options.animate);
+                    }
+                    isDown = false;
+                    isSwipe = false;
+                });
+
+                boxList[i].addEventListener('mousedown', function(e) {
+                    isDown = true;
+                    startX = e.pageX;
+                });
+                boxList[i].addEventListener('mousemove', function(e) {
+                    if (isDown) {
+                        distanceX = e.pageX - startX;
+                        Math.abs(distanceX) > swipeWidth ? isSwipe = true : isSwipe = false;
+                    }
+                });
+                boxList[i].addEventListener('mouseup', function(e) {
+                    if (isSwipe) {
+                        console.log('swipe:' + distanceX);
+                        distanceX > 0 ? self.animate(0, self.options.animate) : self.animate(1, self.options.animate);
+                    }
+                    isDown = false;
+                    isSwipe = false;
+                });
+            }
+        },
+        animate: function(duration, _type) {
+            var self = this;
+            if (this.animated || (duration == 1 && this.attr.next >= this.attr._length) || (duration == 0 && this.attr.pre <= -1)) {
+                return;
+            }
+            this.animated = true;
+            if (duration) {
+                this.reset(this.attr.current, _type).className += (' '+ _type + '-current-pre '); // 执行动画，展示页沦为上一页
+                this.el.boxList[this.attr.next].className += (' '+ _type + '-next-current '); // 执行动画，下一页逆袭为展示页
+            } else {
+                this.reset(this.attr.current, _type).className += (' '+ _type + '-current-next '); // 执行动画，展示页沦为下一页
+                this.el.boxList[this.attr.pre].className += (' '+ _type + '-pre-current '); // 执行动画，上一页逆袭为展示页
+            }
+            setTimeout(function() {
+                self.reset(self.attr.current, _type); // 页面执行完动画进行复位
+                self.changeIndex(duration);
+                self.el.boxList[self.attr.current].className += ' show ';
+                self.setArrow();
+                self.animated = false;
+            }, this.options.speed);
+        },
+        reset: function(index, _type) {
+            this.el.boxList[index].className = this.pageStyle;
+            return this.el.boxList[index];
+        },
+        setArrow: function () { // 判断箭头是否显示
+            (this.attr.next >= this.attr._length) ? this.el.nextButton.style.display = 'none' : this.el.nextButton.style.display = 'block';
+            (this.attr.pre <= -1) ? this.el.preButton.style.display = 'none' : this.el.preButton.style.display = 'block';
+        },
+        changeIndex: function(duration) {
+            if (duration) {
+                ++this.attr.current;
+                ++this.attr.next;
+                ++this.attr.pre;
+            } else {
+                --this.attr.current;
+                --this.attr.next;
+                --this.attr.pre;
+            }
+        },
+        getSpeed: function(speed) {
+            switch (speed) {
+                case 'x-slow':
+                    this.options.speed = 3200;
+                    break;
+                case 'slow':
+                    this.options.speed = 1200;
+                    break;
+                case 'normal':
+                    this.options.speed = 800;
+                    break;
+                case 'fast':
+                    this.options.speed = 560;
+                    break;
+                case 'x-fast':
+                    this.options.speed = 320;
+                    break;
+                default:
+                    this.options.speed = 800;
+                    break;
+            }
+        }
+    }
+    var pptboxList = document.querySelectorAll('.pptbox');
+    for (var i = 0; i < pptboxList.length; i++) {
+        new PPTBox({
+            animate: pptboxList[i].getAttribute('animate'),
+            speed: pptboxList[i].getAttribute('speed')
+        }, pptboxList[i]);
+    }
+})();
